f89b17b99ee2b89065c7b27412996cdc5fe53fbf
potatameister <180633607+potatameister@users.noreply.github.com>
2026-03-07 22:23:59 +0700

    Fix resource conflicts: Add packaging exclusions for Netty/Ktor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/INDEX.LIST"
+            excludes += "META-INF/io.netty.versions.properties"
         }
     }
 }
